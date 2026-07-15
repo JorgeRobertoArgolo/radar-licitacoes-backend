e17700bb9857351cf949dd624462af87d2871af6
Jorge Roberto Argolo <jorgerobertoargolo@gmail.com>
2026-07-15 10:34:26 -0300

    :wrench: config: configuração inicial do projeto
    Criando o projeto pelo Spring Initializer e configurado o .gitignore e o pom.xml.

---
+++ src/main/java/br/com/jorgeargolo/radar_licitacoes_backend/RadarLicitacoesBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.jorgeargolo.radar_licitacoes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadarLicitacoesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadarLicitacoesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jorgeargolo/radar_licitacoes_backend/RadarLicitacoesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jorgeargolo.radar_licitacoes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadarLicitacoesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
